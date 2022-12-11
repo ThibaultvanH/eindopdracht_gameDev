c98d82ddc39de4ee12000b0ab67e0aef9bfe3ea6
Thibault Van Hellemont <74325209+ThibaultvanH@users.noreply.github.com>
2022-12-11 13:12:50 +0100

    basic menu ui

--- eindopdracht/Game1.cs
+++ eindopdracht/Game1.cs
@@ -7,35 +7,44 @@ using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-
-
 namespace eindopdracht
 {
+
+    public enum GameState
+    {
+    Menu,
+    level1,
+    level2,
+    gameover   
+    }
+
     internal class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
-        private GraphicsDevice _device;
-
         private SpriteBatch _spriteBatch;
+
+
         public static List<Rectangle> grassup = new List<Rectangle>();
         public static List<Rectangle> grassdown = new List<Rectangle>();
         public static List<Rectangle> grassright = new List<Rectangle>();
         public static List<Rectangle> grassleft = new List<Rectangle>();
-
-
-
+        public static List<Block> blocks = new List<Block>();
 
         private Texture2D texture;
         private Texture2D bloktexture;
         private Texture2D Blocktexture;
         private Texture2D BG;
         private Texture2D greenmantexture;
+
+
         int[,] curentlevel = level.level1;
-        public static List<Block> blocks = new List<Block>();
+        public static GameState Gamestate = GameState.Menu;
+        
         BlockFactory blockFactory = new BlockFactory();
         Hero hero;
         greenman greenman1;
-        
+        Button button;
+   
 
         public Game1()
         {
@@ -49,11 +58,33 @@ namespace eindopdracht
         {
             // TODO: Add your initialization logic here
 
-            
-            loadTextures();
-            InitializeGameObjects();           
+            loadBG();
+            loadMenu();
+            loadGame();
+
             base.Initialize();
 
+        }
+
+        private void loadBG()
+        {
+            BG = Content.Load<Texture2D>("BG");
+        }
+
+        private void loadMenu()
+        {
+            button = new Button(Content.Load<Texture2D>("Control/wooden"), Content.Load<SpriteFont>("font/newfont"))
+            {
+                
+                Text = "Start Game",
+            }; 
+            button.Position = new Vector2((GraphicsDevice.Viewport.Width - button.texture.Width) / 2, 150);
+        }
+
+        private void loadGame()
+        {
+            loadTextures();
+            InitializeGameObjects();
         }
 
         private void loadTextures()
@@ -62,7 +93,7 @@ namespace eindopdracht
             bloktexture = new Texture2D(GraphicsDevice, 1, 1);
             bloktexture.SetData(new Color[] { Color.DarkSlateGray });
             Blocktexture = Content.Load<Texture2D>("Tile-svg3");
-            BG = Content.Load<Texture2D>("BG");
+            
             greenmantexture = Content.Load<Texture2D>("Walk");
             
         }
@@ -89,27 +120,62 @@ namespace eindopdracht
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-
-            if (greenman1?.health < 0)
+            switch (Gamestate)
             {
-                greenman1 = null;
+                case GameState.Menu:
+                    button?.Update(gameTime);
+
+                    break;
+
+
+                case GameState.level1:
+                case GameState.level2:
+                    if (greenman1?.health < 0)
+                    {
+                        greenman1 = null;
+                    }
+                    hero.Update(gameTime);
+                    greenman1?.Update(gameTime);
+                    break;
+
+
+                case GameState.gameover:
+                    break;
+                default:
+                    break;
             }
-            hero.Update(gameTime);
-            greenman1?.Update(gameTime);
             base.Update(gameTime);
 
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+
             _spriteBatch.Begin();
-            _spriteBatch.Draw(BG,new Rectangle(0,-100,1000,750),Color.White);
-            hero.Draw(_spriteBatch);
-            greenman1?.Draw(_spriteBatch);
-            foreach (Block block in blocks)
+            _spriteBatch.Draw(BG, new Rectangle(0, -100, 1000, 750), Color.White);
+            switch (Gamestate)
             {
-                block?.Draw(_spriteBatch);
+                case GameState.Menu:
+                    button.Draw(gameTime, _spriteBatch);
+                    break;
+
+
+                case GameState.level1:
+                case GameState.level2:
+                    
+                    hero.Draw(_spriteBatch);
+                    greenman1?.Draw(_spriteBatch);
+                    foreach (Block block in blocks)
+                    {
+                        block?.Draw(_spriteBatch);
+                    }
+                    break;
+
+
+                case GameState.gameover:
+                    break;
+                default:
+                    break;
             }
             _spriteBatch.End();
             base.Draw(gameTime);
